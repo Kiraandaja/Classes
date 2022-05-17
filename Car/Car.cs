650998ab1b7e6c73468b009d1a2c06b7a21f7a98
Kiraandaja <kiraandaja@gmail.com>
2022-05-17 08:39:00 -0700

    second commit

--- Car/Car.cs
+++ Car/Car.cs
@@ -6,5 +6,20 @@ namespace Car
         public Car()
         {
         }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public void MakeEngineNoise(string engineNoise)
+        {
+            Console.WriteLine(engineNoise);
+        }
+        public void MakeHonkNoise(string honkNoise)
+        {
+            Console.WriteLine(honkNoise);
+        }
     }
 }
--- Car/Program.cs
+++ Car/Program.cs
@@ -6,7 +6,25 @@ namespace Car
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var takirasCar = new Car();
+            takirasCar.Make = "Audi";
+            takirasCar.Make = "Audi";
+            takirasCar.Model = "Q8";
+            takirasCar.Year = 2022;
+            takirasCar.EngineNoise = "vroom";
+            takirasCar.HonkNoise = "beep";
+
+            var breahsCar = new Car()
+            {
+                Year = 2019,
+                Make = "Tesla",
+                Model = "Cyber Truck",
+                EngineNoise = "...",
+                HonkNoise = "brmmp",
+                IsDriveable = false
+            };
+
         }
     }
 }
+
